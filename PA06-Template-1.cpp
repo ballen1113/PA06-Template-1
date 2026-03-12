5ebdec13baacf375abec82b0de09b8718020cf92
L Carver <wifihum@gmail.com>
2026-03-11 22:17:39 -0700

    Add project files.

---
+++ PA06-Template-1.cpp
@@ -0,0 +1,61 @@
+//------------------------------------------------------------------------------
+// PA06-Template-1.cpp
+// 
+// -Generates random values between passed high and low, inclusive
+// -Counts occurrences of each random value
+// -Displays values and counts
+//------------------------------------------------------------------------------
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <vector>
+
+//------------------------------------------------------------------------------
+// generates random numbers and displays occurrence count of each
+//------------------------------------------------------------------------------
+void countOccurrences(int low, int high, size_t nElements)
+{
+    // data vector with nElements elements all set to 0
+    std::vector<int> vData(nElements, 0);
+
+    int modBy = high + 1 - low;
+
+    // fill data vector with random elements of type T between (low, high)
+    for (size_t i = 0; i < nElements; i++) {
+        int rando = rand() % modBy + low;
+        vData.at(i) = rando;
+    }
+
+    // count vector with nElements elements all set to 0
+    std::vector<size_t> vCounts(nElements, 0);
+
+    // count occurrences of each vector element
+    for (size_t i = 0; i < nElements; i++) {
+        // value to search vData for
+        int element = vData.at(i);
+
+        for (size_t k = 0; k < nElements; k++) {
+            if (vData.at(k) == element)
+                vCounts.at(k)++;
+        }
+    }
+
+    // display each vector element and how many times it occurs in vector
+    for (size_t i = 0; i < nElements; i++) {
+        std::cout << "Element: " << vData.at(i)
+            << " occurs " << vCounts.at(i) << " times\n";
+    }
+}
+
+//------------------------------------------------------------------------------
+// entry point
+//------------------------------------------------------------------------------
+int main()
+{
+    // seed random number generator
+    srand(static_cast<unsigned int>(time(0)));
+
+    countOccurrences(10, 20, 4);
+
+    return 0;
+}
